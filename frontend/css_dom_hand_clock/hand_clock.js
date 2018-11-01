3ea6aad18d86c782e07766a704846eae8839d324
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2018-10-31 22:21:08 -0700

    hand_clock.js - creates shadow DOM and clock elements

--- frontend/css_dom_hand_clock/hand_clock.js
+++ frontend/css_dom_hand_clock/hand_clock.js
@@ -2,5 +2,13 @@ class HandClock extends HTMLElement {
   constructor() {
     super();
 
+    // Create shadow DOM and clock elements
+    const shadow = this.attachShadow({mode: 'open'});
+    const clockFace = document.createElement('div');
+    const hourHand = document.createElement('div');
+    const minuteHand = document.createElement('div');
+    const secondHand = document.createElement('div');
+    const style = document.createElement('style');
+    const size = this.getAttribute('size');
   }
 }
