d69e744b18013838aeb748a85789a5f29b2d4999
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2018-10-31 22:22:02 -0700

    hand_clock.js - adds textContent to style element and styles clock-face

--- frontend/css_dom_hand_clock/hand_clock.js
+++ frontend/css_dom_hand_clock/hand_clock.js
@@ -16,5 +16,27 @@ class HandClock extends HTMLElement {
     minuteHand.setAttribute('class', 'minute hand');
     secondHand.setAttribute('class', 'second hand');
 
+    /*
+      Display clock face using flex to easily center hands.
+      Stack hands using absolute and rotate them all to an upright position.
+      Use ::after element selector on each hand to create their visual components.
+      This allows the base element to be the center of rotation.
+      Specify a height and width for all hands based on size attribute.
+    */
+    style.textContent = `
+      * {
+        box-sizing: border-box;
+      }
+
+      .clock-face {
+        align-items: center;
+        justify-content: center;
+        border: 2px solid black;
+        border-radius: 50%;
+        display: flex;
+        height: ${size}px;
+        width: ${size}px;
+      }
+    `
   }
 }
