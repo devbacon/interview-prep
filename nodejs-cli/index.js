8016447807a205397e78c627e0bc3a9b0295bfa1
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2018-11-20 11:05:38 -0800

    initialized index.js to create a node.js cli tool

---
+++ nodejs-cli/index.js
@@ -0,0 +1,4 @@
+// While preforming several code interviews, I have been asked to write
+// NodeJS code that acts as a CLI. I have not spent much time using
+// NodeJS in this way so I think it's about time to make a NodeJS CLI.
+
